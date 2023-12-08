be1c7b676eac839df072454ba70aca378c51686d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-12-08 15:25:37 +0100

    learnt about components

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -2,8 +2,22 @@ import React from "react";
 
 //Create a component
 
-function Greetings () {
-  return (<p>Hello Good Morning!</p>)
+function Greetings() {
+  return <p>Hello Good Morning!</p>;
+}
+
+function Avatar() {
+  return <img src="/fb.png" alt="profile" />;
+}
+function UserProfileCard() {
+  return (
+    <div className="user-profile-card">
+      <Avatar />
+
+      <h2>Al-Areef</h2>
+      <p>Full stack dev</p>
+    </div>
+  );
 }
 
 const App = () => {
@@ -11,6 +25,7 @@ const App = () => {
     <section className="header">
       <h1>Hello World!</h1>
       <Greetings />
+      <UserProfileCard />
     </section>
   );
 };
