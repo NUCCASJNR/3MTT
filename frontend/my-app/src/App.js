7bbb530abc0515c98e357734907de58867b3dbff
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-12-09 10:01:09 +0100

    Still react

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -13,12 +13,21 @@ function UserProfileCard() {
   return (
     <div className="user-profile-card">
       <Avatar />
-
       <h2>Al-Areef</h2>
       <p>Full stack dev</p>
     </div>
   );
 }
+
+const my_stack_lists = ["React", "Node", "MongoDB", "python", "Django", "flask"];
+
+const my_stack_lists_items = my_stack_lists.map((my_stack_list) => (
+  <li>{my_stack_list}</li>
+));
+// A list item component
+// function ListItem() {
+//   return <li>list item</li>;
+// }
 
 const App = () => {
   return (
@@ -26,6 +35,7 @@ const App = () => {
       <h1>Hello World!</h1>
       <Greetings />
       <UserProfileCard />
+      <ul>{my_stack_lists_items}</ul>
     </section>
   );
 };
